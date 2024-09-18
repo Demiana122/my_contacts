fe08bedb6266d5c378de52849dbf77095f49150e
Demiana 122 <demianaseed@gmail.com>
2024-09-18 19:11:48 +0200

    second-branch

--- lib/widgets/SocialMediaIcon.dart
+++ lib/widgets/SocialMediaIcon.dart
@@ -1,16 +1,65 @@
+// import 'dart:io';
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/start.dart';
+// import 'package:url_launcher/url_launcher.dart';
+
+// class SocialMediaIcon extends StatelessWidget {
+//   String SocialMedia;
+//     String SocialMediaLinks;
+//    SocialMediaIcon({
+//     super.key,
+//     required this.SocialMedia,
+//       required this.SocialMediaLinks,
+//   });
+  
+//   Uri? get url => null;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(12.0),
+//       child: InkWell(
+//         child: CircleAvatar(
+//           backgroundColor: Colors.transparent,
+//         backgroundImage: AssetImage('assets/$SocialMedia'),
+//         radius: 40,
+//                         ),
+//           onTap: () {
+//           showModalBottomSheet(
+//             context: context,
+//            builder:(context){
+//             return ElevatedButton(
+//           style: ButtonStyle(
+//             backgroundColor:WidgetStatePropertyAll(Colors.orange)
+//           ),
+//           onPressed: () {
+//             // استخدام url الممرر كمعامل
+//             launchUrl(url!, mode: LaunchMode.externalApplication);
+//           },
+//           child: Text("Start Social Media"),
+//            });
+          
+//   );
+//           },              
+//       ),
+//     );
+//   }
+// }
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/start.dart';
+//import 'package:flutter_application_1/start.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class SocialMediaIcon extends StatelessWidget {
   String SocialMedia;
-    String SocialMediaLinks;
-   SocialMediaIcon({
+  String SocialMediaLinks;
+
+  SocialMediaIcon({
     super.key,
     required this.SocialMedia,
-      required this.SocialMediaLinks,
+    required this.SocialMediaLinks,
   });
 
   @override
@@ -20,16 +69,26 @@ class SocialMediaIcon extends StatelessWidget {
       child: InkWell(
         child: CircleAvatar(
           backgroundColor: Colors.transparent,
-        backgroundImage: AssetImage('assets/$SocialMedia'),
-        radius: 40,
-                        ),
-          onTap: () {
-            Navigator.push(
-    context,
-   MaterialPageRoute(builder: (context) => StartSocialMedia(url: Uri.parse(SocialMediaLinks))),
-
-  );
-          },              
+          backgroundImage: AssetImage('assets/$SocialMedia'),
+          radius: 40,
+        ),
+        onTap: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:WidgetStatePropertyAll(Colors.orange)
+                ),
+                onPressed: () {
+                  // استخدام SocialMediaLinks بدلاً من url
+                  launchUrl(Uri.parse(SocialMediaLinks), mode: LaunchMode.externalApplication);
+                },
+                child: Text("Start $SocialMediaLinks"),
+              );
+            },
+          );
+        },
       ),
     );
   }
